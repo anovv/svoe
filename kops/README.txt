986031de1bfc47b5eb39793ca3dbadf9a09e1cfe
dirtyValera <valera.dirty@gmail.com>
2022-01-23 13:11:06 +0300

    [Kops] Multicluster support

--- kops/README.txt
+++ kops/README.txt
@@ -1,42 +1,5 @@
 https://aymen-segni.com/index.php/2020/04/19/deploy-kubernetes-aws-k8s-cluster-with-terraform-and-kops/
 
-Usage (inside kops/ dir)
-
-- Gen cluster.yaml based on template.yaml
-$ TF_OUTPUT_JSON=$(cd ../terraform && terraform output -json)
-$ (echo $TF_OUTPUT_JSON | yq e -P -) > values.yaml
-$ CLUSTER_NAME="$(echo $TF_OUTPUT_JSON | jq -r .cluster_name.value)"
-
-$ kops toolbox template --name ${CLUSTER_NAME} --values values.yaml --template template.yaml --format-yaml > cluster.yaml
-
-- Put kops state in s3
-
-$ STATE="s3://$(echo ${$TF_OUTPUT_JSON} | jq -r .kops_s3_bucket_name.value)"
-
-$ kops replace -f cluster.yaml --state ${STATE} --name ${CLUSTER_NAME} --force
-
-$ kops create secret --name ${CLUSTER_NAME} --state ${STATE} --name ${CLUSTER_NAME} sshpublickey admin -i ~/.ssh/id_rsa.pub
-
-- Create terraform config for cluster
-
-$ kops update cluster \
---out=terraform_out \
---target=terraform \
---state ${STATE} \
---name ${CLUSTER_NAME}
-
-- Apply
-
-$ terraform init
-$ terraform plan
-$ terraform apply
-
-Suggestions:
- * validate cluster: kops validate cluster --wait 10m
- * list nodes: kubectl get nodes --show-labels
- * ssh to the master: ssh -i ~/.ssh/id_rsa ubuntu@api.apn1.k8s.local
- * the ubuntu user is specific to Ubuntu. If not using Ubuntu please use the appropriate user based on your OS.
-
 for certs:
 
 https://stackoverflow.com/questions/46234295/kubectl-unable-to-connect-to-server-x509-certificate-signed-by-unknown-authori
--- kops/gen_cluster_config.sh
+++ kops/gen_cluster_config.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 
+if ! [[ -n "$1" ]]; then
+  echo "Cluster id is not set"
+  exit
+fi
+CLUSTER_ID="\"$1\""
 TF_OUTPUT_JSON=$(cd ../terraform && terraform output -json)
-(echo $TF_OUTPUT_JSON | yq e -P -) > values.yaml
-CLUSTER_NAME="$(echo $TF_OUTPUT_JSON | jq -r .cluster_name.value)"
-STATE="s3://$(echo $TF_OUTPUT_JSON | jq -r .kops_s3_bucket_name.value)"
+CLUSTER_CONFIG=$(echo $TF_OUTPUT_JSON | jq -r .multicluster_config_output.value.$CLUSTER_ID)
+CLUSTER_NAME=$(echo $CLUSTER_CONFIG | jq -r .cluster_name)
+STATE="s3://$(echo $CLUSTER_CONFIG | jq -r .kops_s3_bucket_name)"
 
-echo "Cluster "$CLUSTER_NAME
-echo "Terraform input values in values.yaml"
+OUTPUT_PATH="clusters/${CLUSTER_NAME}"
+mkdir -p $OUTPUT_PATH
+VALUES_OUTPUT_PATH="${OUTPUT_PATH}/values.yaml"
+KOPS_CLUSTER_CONFIG_OUPUT_PATH="${OUTPUT_PATH}/cluster.yaml"
+TEMPLATE_PATH="template.yaml"
+TERRAFORM_OUTPUT_PATH="${OUTPUT_PATH}/terraform/"
+mkdir -p $TERRAFORM_OUTPUT_PATH
+
+(echo $CLUSTER_CONFIG | yq e -P -) > $VALUES_OUTPUT_PATH
+
+echo "Cluster ${CLUSTER_NAME} id ${CLUSTER_ID}"
+echo "Terraform input values in ${VALUES_OUTPUT_PATH}"
 
 kops delete cluster --state $STATE --name $CLUSTER_NAME --yes
 echo "Cleared old state at "$STATE
 
-kops toolbox template --name ${CLUSTER_NAME} --values values.yaml --template template.yaml --format-yaml > cluster.yaml
-echo "Cluster config in cluster.yaml"
+kops toolbox template --name ${CLUSTER_NAME} --values ${VALUES_OUTPUT_PATH} --template ${TEMPLATE_PATH} --format-yaml > ${KOPS_CLUSTER_CONFIG_OUPUT_PATH}
+echo "Kops cluster config in ${KOPS_CLUSTER_CONFIG_OUPUT_PATH}"
 
-kops replace -f cluster.yaml --state $STATE --name $CLUSTER_NAME --force
+kops replace -f ${KOPS_CLUSTER_CONFIG_OUPUT_PATH} --state $STATE --name $CLUSTER_NAME --force
 echo "Kops state moved to "$STATE
 
 kops create secret --name $CLUSTER_NAME --state $STATE --name $CLUSTER_NAME sshpublickey admin -i ~/.ssh/id_rsa.pub
 echo "Created secret sshpublickey"
 
-kops update cluster --out=terraform_out --target=terraform --state $STATE --name $CLUSTER_NAME
-echo "Terraform output in ./terraform_out"
+kops update cluster --out=$TERRAFORM_OUTPUT_PATH --target=terraform --state $STATE --name $CLUSTER_NAME
+echo "Terraform output in ${TERRAFORM_OUTPUT_PATH}"
 echo "Done."
