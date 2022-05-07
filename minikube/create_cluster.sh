a27ba4964532fcccd6138f9ab41acd5271e190d2
dirtyValera <valera.dirty@gmail.com>
2022-05-07 19:38:40 +0300

    [Minikube] create_cluster.sh update

--- minikube/create_cluster.sh
+++ minikube/create_cluster.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-minikube start --nodes 3 -p minikube-1
+minikube start --nodes 3 --extra-config=kubelet.housekeeping-interval=10s -p minikube-1
 ./install_provisioner.sh
 ./label_nodes.sh
