f2dfd49ec3106aa607fd534a7226a7d9beac41d7
dirtyValera <valera.dirty@gmail.com>
2022-05-12 08:50:25 +0300

    [Minikube] Create namespace when creating docker-registry secret

--- minikube/update_docker_registry_secret.sh
+++ minikube/update_docker_registry_secret.sh
@@ -3,7 +3,8 @@
 # TODO use helm chart instead if this is also required for prod/dev remote clusters
 # https://artifacthub.io/packages/helm/architectminds/aws-ecr-credential
 # https://artifacthub.io/packages/helm/zbytes/ecr-creds for multi ns
-kubectl delete secret regcred
+kubectl create namespace data-feed
+kubectl delete secret regcred --namespace=data-feed
 kubectl create secret docker-registry regcred \
   --docker-server=050011372339.dkr.ecr.ap-northeast-1.amazonaws.com \
   --docker-username=AWS \
