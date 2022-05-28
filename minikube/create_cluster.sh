55bf169f86341ed89b1f76b2c0b3ea947127b446
dirtyValera <valera.dirty@gmail.com>
2022-05-28 14:07:18 +0300

    [Minikube] vm.overcommit_memory configuration on cluster creation

--- minikube/create_cluster.sh
+++ minikube/create_cluster.sh
@@ -2,14 +2,19 @@
 
 minikube start --nodes 2 --extra-config=kubelet.housekeeping-interval=10s -p minikube-1
 
-# install volume provisioner
-curl https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml | sed 's/\/opt\/local-path-provisioner/\/var\/opt\/local-path-provisioner/ ' | kubectl apply -f -
-kubectl patch storageclass standard -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
-kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+# memory overcommit configuration # TODO
+#kubectl node-shell minikube-1-m02 -- bash -c "echo 2 > /proc/sys/vm/overcommit_memory && echo 100 > /proc/sys/vm/overcommit_ratio"
 
 # label nodes
 kubectl label nodes minikube-1-m02 workload-type=data-feed
 kubectl label nodes minikube-1-m02 node-type=spot
+
+# TODO add taints?
+
+# install volume provisioner
+curl https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml | sed 's/\/opt\/local-path-provisioner/\/var\/opt\/local-path-provisioner/ ' | kubectl apply -f -
+kubectl patch storageclass standard -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
+kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 
 # auth for ecr docker registry
 ./update_docker_registry_secret.sh
