7f94dc7830c0b6d0a950ecf92cf6333a245297e9
dirtyValera <valera.dirty@gmail.com>
2022-06-23 16:04:47 +0300

    [RE] cAdvisor metrics stats WIP

--- data_feed/perf/metrics/metrics.py
+++ data_feed/perf/metrics/metrics.py
@@ -1,7 +1,7 @@
 import asyncio
 import aiohttp
 
-from perf.defines import PROM, RUN_ESTIMATION_FOR, DATA_FEED_CONTAINER, REDIS_CONTAINER
+from perf.defines import PROM, RUN_ESTIMATION_FOR, DATA_FEED_CONTAINER, REDIS_CONTAINER, REDIS_EXPORTER_CONTAINER
 
 
 def fetch_metrics(pod_name, payload_config):
@@ -44,8 +44,8 @@ async def _fetch_metric_async(metric_type, metric_name, metric_query, session):
 
 
 async def _fetch_metrics_async(pod_name, payload_config):
-    health_metrics = _get_health_metrics(pod_name, payload_config)
-    perf_metrics = _get_perf_metrics(pod_name)
+    health_metrics = _get_data_feed_health_metrics_queries(pod_name, payload_config)
+    perf_metrics = _get_perf_kube_metrics_server_queries(pod_name)
     metric_queries = {**health_metrics, **perf_metrics}
     tasks = []
     session = aiohttp.ClientSession()
@@ -61,7 +61,7 @@ async def _fetch_metrics_async(pod_name, payload_config):
     return res
 
 
-def _get_health_metrics(pod_name, payload_config):
+def _get_data_feed_health_metrics_queries(pod_name, payload_config):
     duration = f'[{RUN_ESTIMATION_FOR}s]'
     duration_subquery = f'[{RUN_ESTIMATION_FOR}s:]'
     metrics = {}
@@ -80,14 +80,56 @@ def _get_health_metrics(pod_name, payload_config):
     return metrics
 
 
-def _get_perf_metrics(pod_name):
+def _get_perf_kube_metrics_server_queries(pod_name):
     # https://github.com/olxbr/metrics-server-exporter to export metrics-server to prometheus
     duration = f'[{RUN_ESTIMATION_FOR}s]'
     duration_subquery = f'[{RUN_ESTIMATION_FOR}s:]'
     metrics = {}
-    for container_name in [DATA_FEED_CONTAINER, REDIS_CONTAINER]:
+    for container_name in [DATA_FEED_CONTAINER, REDIS_CONTAINER, REDIS_EXPORTER_CONTAINER]:
         # TODO ',' or ';' separator instead of '_'
         metrics.update({
+            # mem
+            f'kube_metrics_server_mem_absent_{container_name}':
+                f'avg_over_time((max(absent(kube_metrics_server_pods_mem{{pod_name="{pod_name}", pod_container_name="{container_name}"}})) or vector(0)){duration_subquery})',
+            f'kube_metrics_server_mem_avg_{container_name}': f'avg_over_time(kube_metrics_server_pods_mem{{pod_name="{pod_name}", pod_container_name="{container_name}"}}{duration})',
+            f'kube_metrics_server_mem_max_{container_name}': f'max_over_time(kube_metrics_server_pods_mem{{pod_name="{pod_name}", pod_container_name="{container_name}"}}{duration})',
+            f'kube_metrics_server_mem_min_{container_name}': f'min_over_time(kube_metrics_server_pods_mem{{pod_name="{pod_name}", pod_container_name="{container_name}"}}{duration})',
+            f'kube_metrics_server_mem_095_{container_name}': f'quantile_over_time(0.95, kube_metrics_server_pods_mem{{pod_name="{pod_name}", pod_container_name="{container_name}"}}{duration})',
+
+            # cpu
+            f'kube_metrics_server_cpu_absent_{container_name}':
+                f'avg_over_time((max(absent(kube_metrics_server_pods_cpu{{pod_name="{pod_name}", pod_container_name="{container_name}"}})) or vector(0)){duration_subquery})',
+            f'kube_metrics_server_cpu_avg_{container_name}': f'avg_over_time(kube_metrics_server_pods_cpu{{pod_name="{pod_name}", pod_container_name="{container_name}"}}{duration})',
+            f'kube_metrics_server_cpu_max_{container_name}': f'max_over_time(kube_metrics_server_pods_cpu{{pod_name="{pod_name}", pod_container_name="{container_name}"}}{duration})',
+            f'kube_metrics_server_cpu_min_{container_name}': f'min_over_time(kube_metrics_server_pods_cpu{{pod_name="{pod_name}", pod_container_name="{container_name}"}}{duration})',
+            f'kube_metrics_server_cpu_095_{container_name}': f'quantile_over_time(0.95, kube_metrics_server_pods_cpu{{pod_name="{pod_name}", pod_container_name="{container_name}"}}{duration})',
+        })
+
+    return metrics
+
+
+# TODO finish this
+def _get_cadvisor_metrics(pod_name):
+    duration = f'[{RUN_ESTIMATION_FOR}s]'
+    duration_subquery = f'[{RUN_ESTIMATION_FOR}s:]'
+    metrics = {}
+    for container_name in [DATA_FEED_CONTAINER, REDIS_CONTAINER, REDIS_EXPORTER_CONTAINER]:
+        # TODO ',' or ';' separator instead of '_'
+        metrics.update({
+            # TODO explore relevant metrics:
+            # network
+            # container_network_transmit_bytes_total
+            # container_network_transmit_errors_total
+            # container_network_receive_bytes_total
+            # container_network_receive_errors_total
+
+            # TODO https://www.metricfire.com/blog/top-10-cadvisor-metrics-for-prometheus/
+            # https://prometheus.io/docs/guides/cadvisor/
+            # https://www.cloudforecast.io/blog/cadvisor-and-kubernetes-monitoring-guide/
+            # memory
+
+            # cpu
+
             # mem
             f'mem_absent_{container_name}':
                 f'avg_over_time((max(absent(kube_metrics_server_pods_mem{{pod_name="{pod_name}", pod_container_name="{container_name}"}})) or vector(0)){duration_subquery})',
@@ -95,14 +137,6 @@ def _get_perf_metrics(pod_name):
             f'mem_max_{container_name}': f'max_over_time(kube_metrics_server_pods_mem{{pod_name="{pod_name}", pod_container_name="{container_name}"}}{duration})',
             f'mem_min_{container_name}': f'min_over_time(kube_metrics_server_pods_mem{{pod_name="{pod_name}", pod_container_name="{container_name}"}}{duration})',
             f'mem_095_{container_name}': f'quantile_over_time(0.95, kube_metrics_server_pods_mem{{pod_name="{pod_name}", pod_container_name="{container_name}"}}{duration})',
-
-            # cpu
-            f'cpu_absent_{container_name}':
-                f'avg_over_time((max(absent(kube_metrics_server_pods_cpu{{pod_name="{pod_name}", pod_container_name="{container_name}"}})) or vector(0)){duration_subquery})',
-            f'cpu_avg_{container_name}': f'avg_over_time(kube_metrics_server_pods_cpu{{pod_name="{pod_name}", pod_container_name="{container_name}"}}{duration})',
-            f'cpu_max_{container_name}': f'max_over_time(kube_metrics_server_pods_cpu{{pod_name="{pod_name}", pod_container_name="{container_name}"}}{duration})',
-            f'cpu_min_{container_name}': f'min_over_time(kube_metrics_server_pods_cpu{{pod_name="{pod_name}", pod_container_name="{container_name}"}}{duration})',
-            f'cpu_095_{container_name}': f'quantile_over_time(0.95, kube_metrics_server_pods_cpu{{pod_name="{pod_name}", pod_container_name="{container_name}"}}{duration})',
         })
 
     return metrics
