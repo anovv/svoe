e4e6be85b71d5700a21dec56b211abc648dc5fc8
dirtyValera <valera.dirty@gmail.com>
2022-06-24 08:41:48 +0300

    [Prometheus] Patch prom statefulset with --enable-feature=memory-snapshot-on-shutdown for prom container + bump Helm prom version to v2.36.2

--- helm/scripts/patch_kube_prometheus_stack_resources.sh
+++ helm/scripts/patch_kube_prometheus_stack_resources.sh
@@ -7,3 +7,6 @@ kubectl patch daemonset kube-prometheus-stack-prometheus-node-exporter -n monito
 rs_name=$(kubectl get replicaset -l app.kubernetes.io/part-of=kube-state-metrics -n monitoring |  awk '{print $1}' | tail -n +2)
 kubectl patch replicaset $rs_name -n monitoring -p '{"spec":{"template":{"spec":{"containers":[{"resources":{"limits":{"memory":"50Mi", "cpu": "30m"}, "requests":{"memory":"50Mi", "cpu": "30m"}},"name":"kube-state-metrics"}]}}}}'
 
+kubectl patch statefulset prometheus-kube-prometheus-stack-prometheus -n monitoring --type=json -p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--enable-feature=memory-snapshot-on-shutdown"}]'
+
+# TODO restart controllers?
