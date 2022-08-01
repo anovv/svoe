6f040b6462ef47fc3147e119ee932501ae37ec73
dirtyValera <valera.dirty@gmail.com>
2022-08-01 10:05:46 +0300

    [Minikube] Fix start script

--- minikube/create_cluster.sh
+++ minikube/create_cluster.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-minikube config set memory 3072
+minikube config set memory 3072 # need to shutdown docker desktop for this to work
 minikube start --nodes 2 --extra-config=kubelet.housekeeping-interval=10s -p minikube-1
 
 # memory overcommit configuration
 #kubectl node-shell minikube-1-m02 -- bash -c "echo 2 > /proc/sys/vm/overcommit_memory && echo 100 > /proc/sys/vm/overcommit_ratio"
 
 # label nodes
-kubectl label nodes minikube-1-m03 workload-type=data-feed
-kubectl label nodes minikube-1-m03 node-type=spot
-kubectl label nodes minikube-1-m03 svoe-role=resource-estimator
+kubectl label nodes minikube-1-m02 workload-type=data-feed
+kubectl label nodes minikube-1-m02 node-type=spot
+kubectl label nodes minikube-1-m02 svoe-role=resource-estimator
 
 # taints
-kubectl taint nodes minikube-1-m03 node-type=spot:NoSchedule
-kubectl taint nodes minikube-1-m03 workload-type=data-feed:NoSchedule
-kubectl taint nodes minikube-1-m03 svoe-role=resource-estimator:NoSchedule
+kubectl taint nodes minikube-1-m02 node-type=spot:NoSchedule
+kubectl taint nodes minikube-1-m02 workload-type=data-feed:NoSchedule
+kubectl taint nodes minikube-1-m02 svoe-role=resource-estimator:NoSchedule
 
 # patch kube-proxy to be QoS Guaranteed
 kubectl patch daemonset kube-proxy -n kube-system -p '{"spec":{"template":{"spec":{"containers":[{"resources":{"limits":{"memory":"50Mi", "cpu": "40m"}, "requests":{"memory":"50Mi", "cpu": "40m"}},"name":"kube-proxy"}]}}}}'
