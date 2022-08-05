21d208c789e56a3581c1a315104a8bd627dc0f3a
dirtyValera <valera.dirty@gmail.com>
2022-08-05 08:47:55 +0300

    [RE][analyzer] plot df channels health histograms

--- data_feed/perf/resources-estimation-out/result_analyzer.py
+++ data_feed/perf/resources-estimation-out/result_analyzer.py
@@ -1,7 +1,10 @@
 import json
 import os
 import datetime
+import pandas as pd
 import plotly.graph_objects as go
+import plotly.express as px
+
 from plotly.subplots import make_subplots
 
 from cryptofeed.symbols import str_to_symbol, Symbol
@@ -53,8 +56,7 @@ class REResultAnalyzer:
         return no_metrics
 
     # groups data by exchange.instrument_type for each symbols group
-    def grouped_by_exchange(self, symbol_distribution=UNKNOWN_SYMBOL_DISTRIBUTION):
-        # all_symbol_groups = self.get_all_symbol_groups(symbol_distribution)
+    def grouped_perf_metrics(self, symbol_distribution=UNKNOWN_SYMBOL_DISTRIBUTION):
         grouped = {}
         for _key in self.data[symbol_distribution]:
             pod_data = self.data[symbol_distribution][_key]
@@ -85,7 +87,7 @@ class REResultAnalyzer:
         exchange_instruments = set()
         subplot_titles = []
         for symbol_distribution in distr_strategies:
-            grouped_by_exchange = self.grouped_by_exchange(symbol_distribution)
+            grouped_by_exchange = self.grouped_perf_metrics(symbol_distribution)
             for exchange_instrument in grouped_by_exchange:
                 exchange_instruments.add(exchange_instrument)
 
@@ -98,7 +100,7 @@ class REResultAnalyzer:
         row = 1
         for symbol_distribution in distr_strategies:
             col = 1
-            grouped_by_exchange = self.grouped_by_exchange(symbol_distribution)
+            grouped_by_exchange = self.grouped_perf_metrics(symbol_distribution)
             for exchange_instrument in exchange_instruments:
                 symbol_groups_str = []
                 metric_values = []
@@ -115,10 +117,89 @@ class REResultAnalyzer:
         fig.update_layout(title_text=f'Memory consumption (aggregation={AGG})', autosize=True, width=2000, height=2000)
         fig.show()
 
+    # groups data by exchange.instrument_type+channel for each symbol
+    def grouped_health_metrics(self, symbol_distribution=UNKNOWN_SYMBOL_DISTRIBUTION):
+        grouped = {}
+        for _key in self.data[symbol_distribution]:
+            pod_data = self.data[symbol_distribution][_key]
+            payload_config = pod_data['payload_config']
+            exchange = list(payload_config.keys())[0]
+            for channel in payload_config[exchange].keys():
+                symbols_str = payload_config[exchange][channel]
+                symbols = list(map(lambda s: str_to_symbol(s), symbols_str))
+                for symbol in symbols:
+                    instrument_type = symbol.type
+                    key = f'{exchange}.{instrument_type}'
+                    item = [symbol.normalized] # symbol + absent + avg
+                    if 'metrics' not in pod_data:
+                        item.extend([-1, -1])
+                    else:
+                        health_metrics = pod_data['metrics']['df_health'][channel][symbol.normalized]
+                        if health_metrics['absent'][0] is not None:
+                            item.append(float(health_metrics['absent'][0]))
+                        else:
+                            item.append(-1)
+
+                        if health_metrics['avg'][0] is not None:
+                            item.append(float(health_metrics['avg'][0]))
+                        else:
+                            item.append(-1)
+                    if key in grouped:
+                        if channel in grouped[key]:
+                            grouped[key][channel].append(item)
+                        else:
+                            grouped[key][channel] = [item]
+                    else:
+                        grouped[key] = {
+                            channel: [item]
+                        }
+
+        return grouped
+
+    def plot_health(self, symbol_distr):
+        start = -1.1
+        end = 1.1
+        step = 0.1
+        subplot_titles = []
+        grouped = self.grouped_health_metrics(symbol_distr)
+        rows = len(grouped)
+        cols = 0
+        for exch in grouped:
+            cols = max(cols, len(grouped[exch]))
+
+        for exch in grouped:
+            for channel in grouped[exch]:
+                subplot_titles.append(f'{exch} {channel}')
+            if len(grouped[exch]) < cols:
+                # pad with empty titles
+                for _ in range(cols - len(grouped[exch])):
+                    subplot_titles.append('')
+
+        fig = make_subplots(rows=rows, cols=cols, subplot_titles=subplot_titles)
+        row = 1
+        for exch in grouped:
+            col = 1
+            for channel in grouped[exch]:
+                absent = list(map(lambda e: e[1], grouped[exch][channel]))
+                avg = list(map(lambda e: e[2], grouped[exch][channel]))
+                fig.add_trace(
+                    go.Histogram(x=avg, name=f'{exch}.{channel}', xbins=dict(
+                        start=start,
+                        end=end,
+                        size=step)
+                    ),
+                    row=row, col=col
+                )
+                fig.update_xaxes(range=[start, end], row=row, col=col)
+                col += 1
+            row += 1
+        fig.update_layout(title_text=f'Data Feed Channels Health {symbol_distr}', autosize=True, width=2000, height=1000)
+        fig.show()
+
 
 re = REResultAnalyzer()
 re.load_latest_data()
 # print(re.get_latest_date())
-# print(re.grouped_by_exchange('ONE_TO_ONE')['BINANCE_FUTURES.perpetual'])
+# print(re.grouped_health_metrics('ONE_TO_ONE'))
 # print(re.no_metrics())
-re.plot_mem()
+re.plot_health('ONE_TO_ONE')
