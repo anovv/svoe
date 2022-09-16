02d5b79004dce5bf16ca65bac8e05a4fbe575498
dirtyValera <valera.dirty@gmail.com>
2022-09-16 08:44:01 +0300

    [Dask][AWS] Bump dask-cloudprovider and use farget_spot flag for Dask cluster

--- dask_cluster/dask_fargate.py
+++ dask_cluster/dask_fargate.py
@@ -1,50 +1,10 @@
 from dask_cloudprovider.aws import FargateCluster
 
 
-class HackyDict(dict):
-    # hack lmao
-    # blacklists a 'launchType' key to pass down to Dask Worker/Scheduler config
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.__dict__ = self
-
-    def __setattr__(self, key, value):
-        if key in ['launchType']:
-            return  # silent fail
-        else:
-            super().__setattr__(key, value)
-
-    def __setitem__(self, key, value):
-        if key in ['launchType']:
-            return  # silent fail
-        else:
-            super().__setitem__(key, value)
-
-    def copy(self):
-        return type(self)(self)
-
-    # def __copy__(self):
-    #     cls = self.__class__
-    #     result = cls.__new__(cls)
-    #     result.__dict__.update(self.__dict__)
-    #     return result
-
-
-class SvoeTestDaskCluster(FargateCluster):
+class SvoeDaskCluster(FargateCluster):
     def __init__(self, **kwargs):
-        capacity_provider_fargate_config = HackyDict({
-            'capacityProviderStrategy': [{
-                'capacityProvider': 'FARGATE'
-            }]
-        })
-
-        capacity_provider_fargate_spot_config = HackyDict({
-            'capacityProviderStrategy': [{
-                'capacityProvider': 'FARGATE_SPOT'
-            }]
-        })
-
         super().__init__(
+            fargate_spot=True,
             cluster_arn="arn:aws:ecs:ap-northeast-1:050011372339:cluster/dask-svoe-test-1",
             vpc="vpc-0b7a5ea241a581217",
             subnets=["subnet-0af71c701fb97aca3"],
@@ -55,12 +15,10 @@ class SvoeTestDaskCluster(FargateCluster):
             scheduler_cpu=512,
             scheduler_mem=1024,
             scheduler_timeout="30 minutes",
-            find_address_timeout=120, # TODO fix https://github.com/dask/dask-cloudprovider/issues/313 to speed up boot time
-            worker_task_kwargs=HackyDict(capacity_provider_fargate_spot_config),
-            scheduler_task_kwargs=HackyDict(capacity_provider_fargate_config),
+            find_address_timeout=120,
             environment=
             {
-                'EXTRA_PIP_PACKAGES': 'pyarrow s3fs prefect[aws] fastparquet numpy order_book',
+                'EXTRA_PIP_PACKAGES': 'pyarrow s3fs prefect[aws] fastparquet numpy order_book intervaltree',
                 # TODO use prebuild docker image to speed up boot time
                 # TODO set AWS credentials env vars
             },
