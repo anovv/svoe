58ce9879d2ac572556f67621b7198f81ddd660bd
dirtyValera <valera.dirty@gmail.com>
2022-09-28 07:52:13 +0300

    [Featurizer] L2 Snaps Feature Definition WIP 2

--- featurizer/features/definition/l2_snaps/l2_snaps_feature_definition.py
+++ featurizer/features/definition/l2_snaps/l2_snaps_feature_definition.py
@@ -1,90 +1,114 @@
 import functools
-from typing import List, Dict, Optional, Any
+from typing import List, Dict, Optional, Any, Tuple
 from streamz import Stream
 from order_book import OrderBook
 import pandas as pd
-from functools import partial
 from collections import OrderedDict
+from dataclasses import dataclass
+
+
+@dataclass
+class _State:
+    order_book: OrderBook
+    timestamp: float
+    receipt_timestamp: float
+    data_inconsistencies: Dict
+
+
+@dataclass
+class _Event:
+    timestamp: float
+    receipt_timestamp: float
+    delta: bool
+    orders: List[Tuple[str, float, float]]  # side, price, size
+
+
+@dataclass
+class _Snapshot:
+    timestamp: float
+    receipt_timestamp: float
+    bids_and_asks: Dict[str, float]
 
 
 # TODO should subclass FeatureDefinition once all fields are figured out
 class L2SnapsFeatureDefinition:
 
-    class _State:
-        def __init__(self):
-            self.order_book = OrderBook()
-            self.prev_timestamp = -1
-            self.cur_timestamp = -1
-            self.data_inconsistencies = {}
-
-    def l2_deltas_to_snaps(self, deltas: pd.DataFrame, depth: Optional[int] = None) -> List[Dict[str, Any]]:
+    def l2_deltas_to_snaps(self, deltas: pd.DataFrame, depth: Optional[int] = None) -> Tuple[List[Dict[str, Any]], Dict[str, Any]]:
         # for reverse trnasform snap->delta see https://github.com/bmoscon/cryptofeed/blob/master/cryptofeed/util/book.py
         updates = deltas.to_dict(into=OrderedDict, orient='index')
-        state = self._State()
+        state = _State(OrderBook(), -1, -1, {})
         snapshots = []
         stream = self._stream(state, snapshots, depth)
         for _, update in updates.items():
             stream.emit(update)
 
-        return snapshots
+        return snapshots, state.data_inconsistencies
+
+    def _get_common_columns(self, deltas: pd.DataFrame) -> Dict[str, Any]:
+        # returns columns and values which should be appended to output df
+        # i.e. date, symbol, exchange, etc.
+        return {} # TODO
+
+    def _parse_events(self, deltas: pd.DataFrame) -> List[_Event]:
+        # parses dataframe into list of events
+        grouped_by_ts = deltas.groupby('timestamp')
+        dfs = [grouped_by_ts.get_group(x) for x in grouped_by_ts.groups]
+        events = []
+        for df in dfs:
+            timestamp = df.iloc[0].timestamp
+            receipt_timestamp = df.iloc[0].receipt_timestamp
+            delta = df.iloc[0].delta
+            orders = []
+            df_dict = df.to_dict(into=OrderedDict, orient='index')
+            for v in df_dict.values():
+                orders.append((v['side'], v['price'], v['size']))
+            events.append(_Event(timestamp, receipt_timestamp, delta, orders))
+
+        return events
 
     def _stream(self, state: _State, snapshots: List, depth: Optional[int] = None):
-        stream = Stream()
-        stream\
+        stream = Stream()\
             .accumulate(self._update_state, start=state)\
-            .sink(functools.partial(self._append_snapshot, state=state, snapshots=snapshots, depth=depth))
+            .map(functools.partial(self._state_snapshot, depth=depth))
+            # .sink(functools.partial(self._append_snapshot, state=state, snapshots=snapshots, depth=depth))
 
         return stream
 
-    def _update_state(self, state: _State, update: Dict):
-        timestamp = update['timestamp']
-        side = update['side']
-        price = update['price']  # TODO use Decimals?
-        size = update['size']
-        if size == 0.0:
-            if price in state.order_book[side]:
-                del state.order_book[side][price]
+    @staticmethod
+    def _update_state(state: _State, event: _Event):
+        if not event.delta:
+            # reset order book
+            state.order_book = OrderBook()
+        for side, price, size in event.orders:
+            if size == 0.0:
+                if price in state.order_book[side]:
+                    del state.order_book[side][price]
+                else:
+                    inconsistency_type = 'no_price_with_zero_size'
+                    state.data_inconsistencies[inconsistency_type] = state.data_inconsistencies.get(inconsistency_type, 0) + 1
             else:
-                inconsistency_type = 'no_price_with_zero_size'
-                state.data_inconsistencies[inconsistency_type] = state.data_inconsistencies.get(inconsistency_type, 0) + 1
-        else:
-            state.order_book[side][price] = size
+                state.order_book[side][price] = size
 
-        state.prev_timestamp = state.cur_timestamp
-        state.cur_timestamp = timestamp
+        state.timestamp = event.timestamp
+        state.receipt_timestamp = event.receipt_timestamp
 
     @staticmethod
-    def _order_book_to_snapshot(order_book: OrderBook, depth: Optional[int] = None):
-        snapshot = {}
-
+    def _state_snapshot(state: _State, depth: Optional[int] = None) -> _Snapshot:
+        bids_and_asks = {}
         if depth is None:
-            depth = max(len(order_book.bids), len(order_book.asks)) - 1
+            depth = max(len(state.order_book.bids), len(state.order_book.asks)) - 1
 
         for level in range(depth + 1):
             for side in ['bid', 'ask']:
                 if side == 'bid':
-                    orders = order_book.bids
+                    orders = state.order_book.bids
                 else:
-                    orders = order_book.asks
+                    orders = state.order_book.asks
                 price, size = None, None
                 if level < len(orders):
                     price = orders.index(level)[0]
                     size = orders[price]
-                snapshot[f'{side}[{level}].price'] = price
-                snapshot[f'{side}[{level}].size'] = size
+                bids_and_asks[f'{side}[{level}].price'] = price
+                bids_and_asks[f'{side}[{level}].size'] = size
 
-        return snapshot
-
-    def _append_snapshot(self, state: _State, snapshots: List, depth: Optional[int] = None):
-        # append only if timestamp has changed
-        # (when delta==False each update has same timestamp meaning we reconstruct full order book
-        # and we want to snapshot it only when it is fully ready)
-        # TODO this will not count full snap, only full snap + first delta
-
-        if state.prev_timestamp == state.cur_timestamp or state.prev_timestamp == -1:
-            return
-
-        snapshot = self._order_book_to_snapshot(state.order_book, depth)
-        snapshot['timestamp'] = state.cur_timestamp
-        snapshots.append(snapshot)
-
+        return _Snapshot(state.timestamp, state.receipt_timestamp, bids_and_asks)
