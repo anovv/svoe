c47a0bf936eb462d0213ce70480d2ff4910e24ba
dirtyValera <valera.dirty@gmail.com>
2022-11-07 13:44:42 +0300

    [Ray] set un runtime env for remote cluster

--- ray_cluster/datasource/svoe_datasource.py
+++ ray_cluster/datasource/svoe_datasource.py
@@ -1,7 +1,7 @@
 from typing import Any, Dict, List, Optional
 from ray.data.datasource.datasource import Datasource, Reader
 from ray_cluster.datasource.svoe_datasource_reader import SvoeDatasourceReader
-
+# TODO use https://github.com/matplotlib/mplfinance
 
 class SvoeDatasource(Datasource):
     def create_reader(
--- ray_cluster/test.py
+++ ray_cluster/test.py
@@ -4,8 +4,10 @@
 
 import ray
 import time
+import featurizer
+import ray_cluster
+import os
 
-ADDRESS = 'ray://127.0.0.1:10001'
 
 @ray.remote
 class Counter:
@@ -18,8 +20,35 @@ class Counter:
     def incr(self, value):
         self.i += value
 
-
-ray.init(address=ADDRESS)
+# ray.util.disconnect()
+# ray.util.connect # TODO ?
+ray.init('ray://127.0.0.1:10001', runtime_env={
+    'py_modules': [featurizer, ray_cluster],
+    # TODO figure out deps
+    'pip': [
+        'pyarrow',
+        's3fs',
+        'fastparquet',
+        'order-book',
+        'awswrangler',
+        'boto3',
+        'streamz',
+        'frozenlist',
+        'prefect_aws',
+        'prefect_dask',
+        'prefect_aws',
+        'dask',
+        'tqdm',
+        'matplotlib',
+        'intervaltree'
+    ],
+    'env_vars': {
+        'AWS_ACCESS_KEY_ID': os.environ['AWS_KEY'],
+        'AWS_SECRET_ACCESS_KEY': os.environ['AWS_SECRET'],
+        'AWS_DEFAULT_REGION': 'ap-northeast-1'
+    },
+})
+ray.get_runtime_context()
 c = Counter.remote()
 
 for _ in range(10):
