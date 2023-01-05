fdd8d560c3bf2fafff011a18da318634607e2e35
dirtyValera <valera.dirty@gmail.com>
2023-01-05 15:31:46 +0300

    [Ray][Dask-on-Ray][Distributed Feature DAG Processing] Traverse feature tree postorder + according FeatureDefinition class updates + windowed features WIP

--- featurizer/features/definitions/feature_definition.py
+++ featurizer/features/definitions/feature_definition.py
@@ -1,20 +1,40 @@
 from streamz import Stream
+from typing import Dict, List, Tuple
+from ta.utils import dropna
+from ta.volatility import BollingerBands
+from __future__ import annotations
 
 
 # Represent a feature schema to be used with different params (exchanges, symbols, instrument_types, etc)
 # each set of params producing materialized feature
 class FeatureDefinition:
 
+    @classmethod
+    def type(cls) -> str:
+        return cls.__name__
+
     @staticmethod
-    def stream(upstream: Stream) -> Stream:
+    def stream(dep_upstreams: Dict[str, Stream]) -> Stream:
         raise ValueError('Not Implemented')
-    # channels used to calculate this feature
-    # e.g.
-    # {
-    #   'l2_book': 2,
-    #   'ticker': 1
-    # }
-    # means this feature operates with 2 different l2_books and 1 ticker
+
+    @staticmethod
+    def dep_upstreams_schema() -> List[FeatureDefinition]:
+        # upstream dependencies
+        raise ValueError('Not Implemented')
+
+    def dep_upstream_schema_named(self) -> List[Tuple[FeatureDefinition, str]]:
+        # takes care of  cases when feature has multiple dependecies of the same type
+        res = []
+        count_by_type = {}
+        for dep in self.dep_upstreams_schema():
+            if dep.type() in count_by_type:
+                res.append((dep, f'{dep.type()}-{count_by_type[dep.type()]}'))
+                count_by_type[dep.type()] += 1
+            else:
+                res.append((dep, f'{dep.type()}-0'))
+                count_by_type[dep.type()] = 0
+        return res
+
     # def channels_spec(self):
     #     raise ValueError('Not Implemented')
     #
--- ray_cluster/featurizer/calculator.py
+++ ray_cluster/featurizer/calculator.py
@@ -1,32 +1,61 @@
-from typing import List, Union, Tuple, Dict
+from typing import List, Union, Tuple, Dict, Callable
 from featurizer.features.definitions.feature_definition import FeatureDefinition
+from featurizer.features.utils import convert_str_to_seconds
 from streamz import Stream
 from collections import Collection
 from streamz.dataframe import DataFrame
 import dask
 import dask.graph_manipulation
 import pandas as pd
+import functools
+
 
 # feature tree methods
-FeatureTree = str # TODO represent feature dependencies
-FeatureTreeNode = str # TODO represents tree node
-# FeatureName = str # TODO represents calculatable feature
-Data = str # TODO represent data input, should be a FeatureTree leaf
+# class FeatureTreeNode:
+#
+#     def __init__(self):
+#         self.fd = None
+#         self.name = None
+#         self.children = []
 
-def build_feature_tree(fd: FeatureDefinition) -> FeatureTree:
-    return "" # TODO
+Data = FeatureDefinition
+# FeatureTree = FeatureTreeNode # TODO represent feature dependencies
+# # FeatureTreeNode = str # TODO represents tree node
+# # FeatureName = str # TODO represents calculatable feature
+# Data = FeatureTreeNode # TODO represent data input, should be a FeatureTree leaf
+#
+# def build_feature_tree(fd: FeatureDefinition) -> FeatureTree:
+#     root = FeatureTreeNode()
+#     root.name = fd.name()
+#
+#     return root
+#
+# def get_leaves(feature_tree: FeatureTree) -> List[Data]:
+#     return []
+#
+# def traverse_feature_tree(feature_tree: FeatureTree) -> Collection[FeatureTreeNode]:
+#     return []
+#
+# def get_feature(node: FeatureTreeNode) -> FeatureDefinition:
+#     return None # TODO
+#
+# def get_children(node: FeatureTreeNode) -> List[FeatureTreeNode]:
+#     return []
 
-def get_leaves(feature_tree: FeatureTree) -> List[Data]:
+
+def get_leaves(fd: FeatureDefinition) -> List[Data]:
     return []
 
-def traverse_feature_tree(feature_tree: FeatureTree) -> Collection[FeatureTreeNode]:
-    return []
 
-def get_feature(node: FeatureTreeNode) -> FeatureDefinition:
-    return None # TODO
+def postorder(node: FeatureDefinition, callback: Callable, name: str):
+    named_children = node.dep_upstream_schema_named()
+    if len(named_children) == 0:
+        callback(node, name)
+        return
+    for dep_fd, dep_name in named_children:
+        postorder(dep_fd, callback, dep_name)
 
-def get_children(node: FeatureTreeNode) -> List[FeatureTreeNode]:
-    return []
+    callback(node, name)
 
 # catalog methods
 BlockMeta = str # TODO represents s3 file metdata: name, time range, size, etc.
@@ -53,11 +82,26 @@ def get_block_chunk(blocks_meta_map: Dict, fd: FeatureDefinition, start: float, 
 # feature def aux methods
 # some should be moved to FeatureDefinition class
 # TODO move to FeatureDefinition class
-def is_windowed(fd: FeatureDefinition, dep: FeatureDefinition):
+def is_windowed(fd: FeatureDefinition, dep_feature_name: str):
     return True # TODO
 
-def get_window(fd: FeatureDefinition, dep: FeatureDefinition) -> str:
+def get_window(fd: FeatureDefinition, dep_feature_name: str) -> str:
     return '1m'
+
+def get_prev_feature_delayed_funcs(
+    from_ts: float,
+    features_delayed_by_range: List[Tuple[Dict, float, float]],
+    window: str,
+    dep_feature_name: str
+) -> List:
+    res = []
+    beg, end = from_ts - convert_str_to_seconds(window), from_ts
+    for features_delayed_funcs, start_ts, end_ts in features_delayed_by_range:
+        if start_ts >= beg and start_ts <= end:
+            res.append(features_delayed_funcs[dep_feature_name])
+
+    return res
+
 #
 # def get_fd_sampling_strategy(
 #     fd: FeatureDefinition,
@@ -76,7 +120,7 @@ def load_block_if_needed(
 @dask.delayed
 def calculate_feature(
     fd: FeatureDefinition,
-    dep_feature_results: Dict, # maps dep feature to BlockRange
+    dep_feature_results: Dict[str, Block], # maps dep feature to BlockRange
     start_ts: float,
     end_ts: float
 ) -> Block:
@@ -91,9 +135,11 @@ def build_task_graph(
     start_date: str = None,
     end_date: str = None,
 ):
+
+    root_feature_name = f'{fd.type()}-0'
     features_delayed_by_range = [] # tuples of feature_delayed_funcs dict, start_ts, end_ts
-    feature_tree = build_feature_tree(fd)
-    data_leaves = get_leaves(feature_tree)
+    # feature_tree = build_feature_tree(fd)
+    data_leaves = get_leaves(fd)
     blocks_meta_map = {}
     for data in data_leaves:
         blocks_meta_map[data] = get_block_ranges_meta(data, data_params, start_date, end_date)
@@ -105,31 +151,41 @@ def build_task_graph(
         data_range = data_ranges[i]
         start_ts = data_range[0]
         end_ts = data_range[1]
-        for feature_tree_node in traverse_feature_tree(feature_tree):
-            feature = get_feature(feature_tree_node)
-            block_chunk = get_block_chunk(blocks_meta_map, feature, start_ts, end_ts)
-            if isinstance(feature, Data):
+
+        # bottom up/postorder traversal
+
+        # for feature_tree_node in traverse_feature_tree(fd):
+        def tree_traversal_callback(fd: FeatureDefinition, feature_name: str):
+            # feature_name = get_feature_name(fd)
+            block_chunk = get_block_chunk(blocks_meta_map, fd, start_ts, end_ts)
+            if isinstance(fd, Data):
                 # leaves
                 # TODO is start/end needed here?
-                feature_delayed_funcs[feature] = load_block_if_needed(block_chunk[0])
-                continue
+                feature_delayed_funcs[feature_name] = load_block_if_needed(block_chunk[0])
+                return
             dep_feature_delayed_funcs = {}
-            for dep_feature in get_children(feature_tree_node):
+            for dep_fd, dep_feature_name in fd.dep_upstream_schema_named():
                 # [feature_delayed_funcs[dep_feature]
-                if is_windowed(feature, dep_feature):
-                    window = get_window(feature, dep_feature)
-                    dep_delayed_funcs = get_prev_feature_delayed_funcs(features_delayed_by_range, window, dep_feature)
+                # TODO check we have dep_feature_name key
+                if is_windowed(fd, dep_feature_name):
+                    window = get_window(fd, dep_feature_name)
+                    dep_delayed_funcs = get_prev_feature_delayed_funcs(start_ts, features_delayed_by_range, window, dep_feature_name)
                 else:
-                    dep_delayed_funcs = [feature_delayed_funcs[dep_feature]]
-                dep_feature_delayed_funcs[dep_feature] = dep_delayed_funcs
+                    dep_delayed_funcs = [feature_delayed_funcs[dep_feature_name]]
+                # TODO check we have no circular deps
+                dep_feature_delayed_funcs[dep_feature_name] = dep_delayed_funcs
             feature_delayed = calculate_feature(
-                feature,
+                fd,
                 dep_feature_delayed_funcs,
                 start_ts,
                 end_ts
             )
-            feature_delayed_funcs[feature] = feature_delayed
+            # TODO check we have no circular deps
+            feature_delayed_funcs[feature_name] = feature_delayed
+
+        postorder(fd, tree_traversal_callback, root_feature_name)
         features_delayed_by_range.append((feature_delayed_funcs, start_ts, end_ts))
 
-    res = map(lambda f: f[0][feature], features_delayed_by_range) # list of feature delayed funcs for all ranges
+
+    res = map(lambda f: f[0][root_feature_name], features_delayed_by_range) # list of feature delayed funcs for all ranges
     return res
