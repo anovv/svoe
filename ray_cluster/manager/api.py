875f8787342d52114004b7069ccd968c92231d9e
dirtyValera <valera.dirty@gmail.com>
2023-05-24 16:11:43 +0300

    [RayClusterManager] Use jinja templates for CRD generation

--- ray_cluster/manager/api.py
+++ ray_cluster/manager/api.py
@@ -1,12 +1,39 @@
+from typing import Dict, Tuple, List
+
+from jinja2 import Template
+
+RAYCLUSTER_CONF_PATH = 'yaml/raycluster-template.yaml'
+GEN_PATH = 'gen'
 
 class RayClusterManagerApi:
 
-    def __int__(self, kube_ctx: str):
+    def __init__(self, kube_ctx: str):
         self.kube_ctx = kube_ctx
 
-    def build_crd(self, params):
-        # TODO load/parse yamls, set params
-        return {}
+    def template_ray_cluster_crd(
+        self,
+        user_id: str,
+        cluster_name: str,
+        is_minikube: bool,
+        enable_autoscaling: bool,
+        head_cpu: float,
+        head_memory: str,
+        worker_groups: List[Dict],
+    ) -> str:
+        crd = Template(open(RAYCLUSTER_CONF_PATH, 'r').read()).render(
+            user_id=user_id,
+            cluster_name=cluster_name,
+            is_minikube=is_minikube,
+            enable_autoscaling=enable_autoscaling,
+            head_cpu=head_cpu,
+            head_memory=head_memory,
+            worker_groups=worker_groups
+        )
+        temp_file_path = f'{GEN_PATH}/ray-cluster-{user_id}.yaml'
+        # TODO concurrency/lock?
+        with open(temp_file_path, 'w+') as outfile:
+            outfile.write(crd)
+        return temp_file_path
 
     def launch_cluster(self):
         # TODO call kube api
--- ray_cluster/manager/test_api.py
+++ ray_cluster/manager/test_api.py
@@ -1,10 +1,29 @@
 import unittest
 
+from ray_cluster.manager.api import RayClusterManagerApi
+
 
 class TestRayClusterManagerApi(unittest.TestCase):
 
     def test(self):
-        pass
+        api = RayClusterManagerApi('minikube-1')
+        api.template_ray_cluster_crd(
+            user_id='1',
+            cluster_name='test-ray-cluster',
+            is_minikube=True,
+            enable_autoscaling=False,
+            head_cpu=1,
+            head_memory='3Gi',
+            worker_groups=[{
+                'ray_resources': '\'"{\"worker_size_small\": 9999999, \"instance_on_demand\": 9999999}"\'',
+                'replicas': 3,
+                'min_replicas': 0,
+                'max_replicas': 3,
+                'group_name': 'workers',
+                'cpu': 1,
+                'memory': '3Gi'
+            }]
+        )
 
 
 if __name__ == '__main__':
