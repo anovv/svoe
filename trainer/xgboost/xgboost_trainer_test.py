c94b9994c62d8a65aed0c213024a0e33e53a5af3
dirtyValera <valera.dirty@gmail.com>
2023-07-05 11:20:15 +0300

    [Trainer] Use Ray Datasets API to zip test and predictions data

--- trainer/xgboost/xgboost_trainer_test.py
+++ trainer/xgboost/xgboost_trainer_test.py
@@ -28,22 +28,8 @@ class TestXGBoostTrainer(unittest.TestCase):
             refs = Featurizer.get_result_refs()
             # TODO first two values are weird outliers for some reason, why?
             # df = df.tail(-2)
-            # df_with_timestamp = df.copy(deep=True)
-            # df = df.drop(columns=['timestamp'])
-            # train, valid, test = 0.5, 0.2, 0.3 # weights
-            # train_df = df.iloc[:int(train*len(df))]
-            # valid_df = df.iloc[int(train*len(df)): int((train+valid)*len(df))]
-            # test_df_with_labels = df.iloc[int((train+valid)*len(df)):]
-            # test_df = test_df_with_labels[['volatility', 'spread']]
-            #
-            # train_dataset = ray.data.from_pandas(train_df)
-            # valid_dataset = ray.data.from_pandas(valid_df)
-            # test_dataset = ray.data.from_pandas(test_df)
 
-
-            ds = ray.data.from_pandas_refs(refs)
-            ds = ds.drop_columns(cols=['timestamp', 'receipt_timestamp'])
-            train_ds, valid_ds, test_ds = ds.split_proportionately([0.5, 0.2])
+            train_ds, valid_ds, test_ds = ray.data.from_pandas_refs(refs).split_proportionately([0.5, 0.2])
             params = {
                 'tree_method': 'approx',
                 'objective': 'reg:linear',
@@ -51,12 +37,6 @@ class TestXGBoostTrainer(unittest.TestCase):
             }
 
             label_column = 'label_mid_price' # TODO get dynamically
-
-            # print(train_ds.take_all())
-            # print(valid_ds.take_all())
-            # print(test_ds.take_all())
-            # raise
-
             num_workers = 10
             trainer = XGBoostTrainer(
                 scaling_config=ScalingConfig(num_workers=num_workers, use_gpu=False),
@@ -64,27 +44,48 @@ class TestXGBoostTrainer(unittest.TestCase):
                 params=params,
                 # TODO re what valid is used for
                 # https://www.kaggle.com/questions-and-answers/61835
-                datasets={'train': train_ds, 'valid': valid_ds},
-                # preprocessor=preprocessor, # TODO scale features?
-                num_boost_round=5000,
+                datasets={
+                    'train': train_ds.drop_columns(cols=['timestamp', 'receipt_timestamp']),
+                    'valid': valid_ds.drop_columns(cols=['timestamp', 'receipt_timestamp'])
+                },
+                # preprocessor=preprocessor, # TODO scale features? XGBoost does not need scaling
+                num_boost_round=100,
             )
             result = trainer.fit()
             print(result.metrics)
 
-            predictor = XGBoostPredictor.from_checkpoint(result.checkpoint)
-            t = time.time()
-            predicted_one = predictor.predict(pd.DataFrame(test_ds.take(1)).drop(columns=[label_column]))
-            # predicted_one = predictor.predict(test_ds.limit(1))
-            print(f'Predict one in {time.time() - t}s')
+            # predictor = XGBoostPredictor.from_checkpoint(result.checkpoint)
+            # t = time.time()
+            # predicted_one = predictor.predict(pd.DataFrame(test_ds.take(1)).drop(columns=[label_column]))
+            # print(f'Predict one in {time.time() - t}s')
 
             batch_predictor = BatchPredictor.from_checkpoint(
                 result.checkpoint, XGBoostPredictor
             )
             t = time.time()
-            predicted_labels = batch_predictor.predict(test_ds.drop_columns(cols=[label_column]))
+            predicted_labels = batch_predictor.predict(
+                test_ds.drop_columns(cols=[label_column, 'timestamp', 'receipt_timestamp'])
+            )
+
             print(f'Predict in {time.time() - t}s')
 
-            predicted = list(map(lambda e: e['predictions'], predicted_labels.take_all()))
+            p = test_ds.zip(predicted_labels).to_pandas()
+            p = p.tail(-2)
+            p = p.head(10)
+            p.plot(x='timestamp', y=['mid_price', 'label_mid_price', 'predictions'])
+            plt.show()
+            # print(predicted_labels)
+            # print(test_ds)
+            # actual = test_ds.take_all()
+            # print(actual)
+            #
+            # predicted = list(map(lambda e: e['predictions'], predicted_labels.take_all()))
+            # print(predicted)
+            # actual['predicted'] = predicted
+            #
+            # print(actual)
+
+            # print(predicted)
             # actual = test_df_with_labels['mid_price'].values.tolist()
             # test_df_with_labels['predicted_mid_price'] = predicted
             # test_df_with_labels['timestamp'] = df_with_timestamp['timestamp']
