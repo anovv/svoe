c57a9d6e2238a14ae28b36dd30fabc1b19ee7905
dirtyValera <valera.dirty@gmail.com>
2023-08-10 12:30:54 +0800

    [Airflow] Sync dags between configs DB and local filesystem

--- svoe_airflow/gen_dags.py
+++ svoe_airflow/gen_dags.py
@@ -1,7 +1,7 @@
 from dagfactory import load_yaml_dags
 from airflow.configuration import conf as airflow_conf
 from svoe_airflow.db.dags_mysql_client import DagsMysqlClient
-from svoe_airflow.utils import store_configs_to_yaml_files, DEFAULT_DAG_YAML_SUFFIX
+from svoe_airflow.utils import DEFAULT_DAG_YAML_SUFFIX, sync_configs
 
 dags_folder = airflow_conf.get('core', 'dags_folder')
 suffix = DEFAULT_DAG_YAML_SUFFIX # '.yaml'
@@ -9,8 +9,6 @@ suffix = DEFAULT_DAG_YAML_SUFFIX # '.yaml'
 client = DagsMysqlClient()
 confs = client.select_all_configs()
 
-# TODO we should store only diff between whats in DB and whats on disk
-store_configs_to_yaml_files(confs=confs, dags_folder=dags_folder, suffix=suffix)
+sync_configs(confs=confs, dags_folder=dags_folder, suffix=suffix)
 
-# TODO we should also remove dags which are not in DB
 load_yaml_dags(globals_dict=globals(), dags_folder=dags_folder, suffix=[suffix])
--- svoe_airflow/utils.py
+++ svoe_airflow/utils.py
@@ -1,5 +1,7 @@
+import os
+
 import yaml
-from typing import List
+from typing import List, Tuple
 
 from common.common_utils import base64_decode
 from svoe_airflow.db.models import DagConfigEncoded
@@ -14,6 +16,48 @@ def store_configs_to_yaml_files(confs: List[DagConfigEncoded], dags_folder: str,
         # TODO asyncify this
         with open(filename, 'w') as outfile:
             yaml.dump(dag_config, outfile, default_flow_style=False)
+
+
+def delete_files(to_delete: List[str]):
+    for f in to_delete:
+        if os.path.exists(f):
+            os.remove(f)
+
+
+def diff(confs: List[DagConfigEncoded], dags_folder: str, suffix: str) -> Tuple[List[DagConfigEncoded], List[str]]:
+    to_add = []
+    to_delete = []
+    confs_file_names = list(map(lambda e: construct_dag_yaml_path(
+        dags_folder=dags_folder,
+        owner_id=e.owner_id,
+        dag_name=e.dag_name,
+        suffix=suffix
+    ), confs))
+
+    # check present files
+    on_disc = []
+    for file in os.listdir(dags_folder):
+        full_path = os.path.join(dags_folder, file)
+        if os.path.isfile(full_path) and full_path.endswith(suffix):
+            on_disc.append(full_path)
+
+    for f in on_disc:
+        if f not in confs_file_names:
+            to_delete.append(f)
+
+    for i in range(len(confs_file_names)):
+        if confs_file_names[i] not in on_disc:
+            to_add.append(confs[i])
+
+    return to_add, to_delete
+
+
+def sync_configs(confs: List[DagConfigEncoded], dags_folder: str, suffix: str):
+    to_add, to_delete = diff(confs=confs, dags_folder=dags_folder, suffix=suffix)
+    if len(to_add) > 0:
+        store_configs_to_yaml_files(confs=to_add, dags_folder=dags_folder, suffix=suffix)
+    if len(to_delete) > 0:
+        delete_files(to_delete)
 
 
 def construct_dag_yaml_path(dags_folder: str, owner_id: str, dag_name: str, suffix: str) -> str:
