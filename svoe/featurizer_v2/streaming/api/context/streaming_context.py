3a2678e1f2f51d211006e0188e00b7566d4d9095
dirtyValera <valera.dirty@gmail.com>
2024-01-16 09:17:23 +0400

    [Featurizer2][Streaming][API] Implement StreamSink

--- svoe/featurizer_v2/streaming/api/context/streaming_context.py
+++ svoe/featurizer_v2/streaming/api/context/streaming_context.py
@@ -15,51 +15,29 @@ class StreamingContext:
     def __init__(self, job_config: Dict):
         self.job_config = job_config
         self._id_generator = 0
-        self.stream_sink: List[StreamSink] = []
+        self.stream_sinks: List[StreamSink] = []
         self.job_graph: JobGraph
 
     def generate_id(self):
         self._id_generator += 1
         return self._id_generator
 
+    def add_sink(self, stream_sink: StreamSink):
+        self.stream_sinks.append(stream_sink)
+
     def source(self, source_func: SourceFunction):
-        """Create an input data stream with a SourceFunction
-
-        Args:
-            source_func: the SourceFunction used to create the data stream
-
-        Returns:
-            The data stream constructed from the source_func
-        """
-        return StreamSource(source_func)
+        return StreamSource(self, source_func)
 
     def from_values(self, *values):
-        """Creates a data stream from values
-
-        Args:
-            values: The elements to create the data stream from.
-
-        Returns:
-            The data stream representing the given values
-        """
         return self.from_collection(values)
 
     def from_collection(self, values):
-        """Creates a data stream from the given non-empty collection.
-
-        Args:
-            values: The collection of elements to create the data stream from.
-
-        Returns:
-            The data stream representing the given collection.
-        """
         assert values, "values shouldn't be None or empty"
         func = CollectionSourceFunction(values)
         return self.source(func)
 
     def read_text_file(self, filename: str):
-        """Reads the given file line-by-line and creates a data stream that
-        contains a string with the contents of each such line."""
+        # line by line
         func = LocalFileSourceFunction(filename)
         return self.source(func)
 
--- svoe/featurizer_v2/streaming/api/stream/stream_sink.py
+++ svoe/featurizer_v2/streaming/api/stream/stream_sink.py
@@ -1,5 +1,12 @@
+from svoe.featurizer_v2.streaming.api.operator.operator import SinkOperator
 from svoe.featurizer_v2.streaming.api.stream.stream import Stream
 
 
 class StreamSink(Stream):
-    pass
+    def __init__(
+        self,
+        input_stream: Stream,
+        sink_operator: SinkOperator
+    ):
+        super().__init__(input_stream=input_stream, stream_operator=sink_operator)
+        self.streaming_context.add_sink(self)
