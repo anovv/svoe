33344687369c7a87f861a1379d3bdca687a18b4b
dirtyValera <valera.dirty@gmail.com>
2024-01-16 08:54:15 +0400

    [Featurizer2][Streaming][API] JobGraphOptimizer interface

--- svoe/featurizer_v2/streaming/api/job_graph/job_graph_optimizer.py
+++ svoe/featurizer_v2/streaming/api/job_graph/job_graph_optimizer.py
@@ -1,2 +1,11 @@
+from svoe.featurizer_v2.streaming.api.job_graph.job_graph import JobGraph
+
+
 class JobGraphOptimizer:
-    pass
+
+    def __init__(self, job_graph: JobGraph):
+        self.job_graph = job_graph
+
+    def optimize(self) -> JobGraph:
+        # TODO implement optimizations
+        return self.job_graph
